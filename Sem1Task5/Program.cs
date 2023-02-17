a4bef394bb85a6ff068046033e2925a546b8748a
Oleg Shaidulin <olegschaid@gmail.com>
2023-02-17 17:44:28 +0300

    Добавлено решение к 5 задаче

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача №5
+//Напишите программу, которая на вход принимает
+//одно число (N), а на выходе показывает все целые
+//числа в промежутке от -N до N
+
+Console.Write("Введите число = ");
+
+var day = Convert.ToInt64(Console.Readline());
+var negDay = -day;
+
+for (; negDay <= day; negDay++);
+{
+    Console.Write(negDay+ "\t");
+}

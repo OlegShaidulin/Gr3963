3ed2724f596d84bfe1b9fc4f1c619f5abb48f553
Oleg Shaidulin <olegschaid@gmail.com>
2023-02-17 17:57:02 +0300

    Добавлено другое решение к 5 задаче

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -3,12 +3,37 @@
 //одно число (N), а на выходе показывает все целые
 //числа в промежутке от -N до N
 
+//Console.Write("Введите число = ");
+
+//var day = Convert.ToInt64(Console.Readline());
+//var negDay = -day;
+
+//for (; negDay <= day; negDay++);
+//{
+ //   Console.Write(negDay+ "\t");
+//}
+
 Console.Write("Введите число = ");
 
-var day = Convert.ToInt64(Console.Readline());
-var negDay = -day;
+string? InputLine = Console.ReadLine();
 
-for (; negDay <= day; negDay++);
+//проверка чтобы данные не были пустые
+if(InputLine != null)
 {
-    Console.Write(negDay+ "\t");
+    //Парсим певое число
+
+    int inputNumber = int.Parse(InputLine);
+    int startNumber = inputNumber*(-1);
+
+    //Выходное значение
+    string outline = string.Empty;
+    while(startNumber<inputNumber)
+    {
+        outline = outline + startNumber + ", ";
+        startNumber++;
+    }
+    outline = outline +inputNumber;
+
+    //Вывдодим данные в консоль
+    Console.WriteLine(outline);
 }

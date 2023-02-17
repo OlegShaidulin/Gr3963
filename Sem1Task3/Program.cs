1ca83bdfa30aa1e17b50d903acb4cd44284f1d52
Oleg Shaidulin <olegschaid@gmail.com>
2023-02-17 17:27:03 +0300

    Добавлено решение к 3 задаче

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача №3
+//Напишите программу, которая будет выдавать
+//название дня недели по заданному номеру
+
+//Называем номер дня недели
+
+Console.WriteLine("Номер дня недели");
+
+//Консоль читает номер дня недели
+
+string? LineDay = Console.ReadLine();
+
+//Проверяем на пустоту строку
+
+if (LineDay != null);
+{
+    int day = int.Parse(LineDay);
+    string[] dayWeek = new string[7];
+    dayWeek[0] = "Понедельник";
+    dayWeek[1] = "Вторник";
+    dayWeek[2] = "Среда";
+    dayWeek[3] = "Четверг";
+    dayWeek[4] = "Пятница";
+    dayWeek[5] = "Суббота";
+    dayWeek[6] = "Воскресенье";
+
+    Console.WriteLine(dayWeek[day-1]);
+
+}
+
+
